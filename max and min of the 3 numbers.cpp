561eb0faa3abd766b8da05956a3251fec35aaa34
VishnuDath710 <112502899+VishnuDath710@users.noreply.github.com>
2022-11-07 17:41:55 +0530

    Update max and min of the 3 numbers.cpp

--- max and min of the 3 numbers.cpp
+++ max and min of the 3 numbers.cpp
@@ -1,21 +1,6 @@
 #include<stdio.h>
     int main()
     {
-        int a,b,c;
-        printf("Enter the values of a,b,c: ");
-        scanf("%d %d %d",&a, &b, &c);
-        if (a<b && a<c)
-            printf( "Minimum is %d ",a);
-        else if(b<a && b<c)
-            printf(" Minimum is %d ",b);
-        else
-            printf(" Minimum is %d ",c);
-
-        if (a>b && a>c)
-            printf(" Maximum is %d ",a);
-        else if(b>a && b>c)
-            printf(" Maximum is %d ",b);
-        else
-            printf(" Maximum is %d ",c);
+        printf("HI IM THFF JAGU");
     }
 
